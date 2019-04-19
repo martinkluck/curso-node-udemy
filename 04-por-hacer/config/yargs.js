9393b199c1b3509cc92f74cb72e1a56b8ec32623
martinkluck <martinkluck05@gmail.com>
2019-04-19 18:04:52 -0300

    Clase 4

--- 04-por-hacer/config/yargs.js
+++ 04-por-hacer/config/yargs.js
@@ -1,30 +1,25 @@
 const descripcion = {
-    descripcion: {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea por hacer'
-    }
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
 }
 
 const argv = require('yargs')
     .command('crear', 'Crea una nueva tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .command('actualizar', 'Actualiza una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripcion de la tarea por hacer'
-        },
-        completado: {
-            alias: 'c',
-            default: true,
-            desc: 'Marca como completado o pendiente la tarea'
-        }
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar una tarea', {
+        descripcion
     })
     .help()
     .argv;
